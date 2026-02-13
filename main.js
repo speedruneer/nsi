150320727ea32d9db6760c8a483d526b4e545b00
speedruneer <speedruneeroff.pro@gmail.com>
2026-02-13 08:26:47 +0100

    pushed something inconspicuous

--- main.js
+++ main.js
@@ -4,3 +4,17 @@ const layout = document.getElementById("layout");
 button.addEventListener("click", () => {
     layout.classList.toggle("open");
 });
+
+function showVideo() {
+  document.getElementById('video-overlay').style.display = 'flex';
+}
+
+function closeVideo() {
+  const overlay = document.getElementById('video-overlay');
+  const video = document.getElementById('mp4-video');
+  video.pause();
+  video.currentTime = 0;
+  overlay.style.display = 'none';
+}
+
+for (let i = 0;i<10;i++){closeVideo();}
